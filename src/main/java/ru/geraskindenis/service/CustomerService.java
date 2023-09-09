41d1097499c61a0dc8be31f71758f548e8e4532c
Geraskin Denis <fresh7tea@gmail.com>
2023-09-09 12:05:47 +0300

    Add: CustomerService.java WebController.java
    Signed-off-by: Geraskin Denis <fresh7tea@gmail.com>

---
+++ src/main/java/ru/geraskindenis/service/CustomerService.java
@@ -0,0 +1,36 @@
+package ru.geraskindenis.service;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import ru.geraskindenis.dao.CustomerDao;
+import ru.geraskindenis.model.Customer;
+
+@Service
+public class CustomerService {
+	private static final Logger LOGGER = Logger.getLogger(CustomerService.class.getName());
+	private final CustomerDao customerDao;
+
+	private CustomerService(CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+
+	public void createCustomer(Customer customer) {
+		customerDao.save(customer);
+	}
+
+	public List<Customer> findAll() {
+		return (List<Customer>) customerDao.findAll();
+	}
+
+	public Customer findById(long id) {
+		return customerDao.findById(id).orElseThrow(() -> {
+			String message = "The customer id = '" + id + "' not found.";
+			LOGGER.log(Level.WARNING, message);
+			return new RuntimeException(message);
+		});
+	}
+}
---
+++ src/main/java/ru/geraskindenis/web/WebController.java
@@ -0,0 +1,45 @@
+package ru.geraskindenis.web;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import ru.geraskindenis.model.Customer;
+import ru.geraskindenis.service.CustomerService;
+import ru.geraskindenis.web.dto.CustomerDto;
+
+@RestController
+public class WebController {
+	private static final Logger LOGGER = Logger.getLogger(WebController.class.getName());
+	private CustomerService customerService;
+
+	private WebController(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
+	@PostMapping("/customers")
+	public void createCustomer(@Valid @RequestBody CustomerDto customerDto) {
+		LOGGER.info("Create customer request received: " + customerDto);
+		customerService.createCustomer(Customer.builder().setName(customerDto.getName()).build());
+	}
+
+	@GetMapping("/customers")
+	public ResponseEntity<List<CustomerDto>> getAll() {
+		List<Customer> customers = customerService.findAll();
+		List<CustomerDto> customersDto = customers.stream().map(CustomerDto::createCustomerDto).toList();
+		return ResponseEntity.accepted().body(customersDto);
+	}
+
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<CustomerDto> getById(@PathVariable Long id) {
+		Customer customer = customerService.findById(id);
+		return ResponseEntity.accepted().body(CustomerDto.createCustomerDto(customer));
+	}
+}
